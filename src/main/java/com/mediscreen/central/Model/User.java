e30583b5071b57864383945521a1ea5c2c550e82
ErikM06 <murer.erik@gmail.com>
2022-06-14 10:44:14 +0200

    Spring security and user authentication implemented

---
+++ src/main/java/com/mediscreen/central/Model/User.java
@@ -0,0 +1,93 @@
+package com.mediscreen.central.Model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Collection;
+
+@Entity
+@Table (name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column (name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    private String role;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+
+}
---
+++ src/main/java/com/mediscreen/central/configuration/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.mediscreen.central.configuration;
+
+import com.mediscreen.central.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Autowired
+    UserService userService;
+
+    @Bean
+    SecurityFilterChain customSecurityFilter(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+            try {
+                authorize
+                        .antMatchers("/resources/**", "/login").permitAll()
+                        .antMatchers("/admin/**").hasRole("ADMIN")
+                        .antMatchers("/central/**").authenticated()
+                        .anyRequest().authenticated()
+                        .and().exceptionHandling()
+                        .and()
+                        .formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/central/getPatientList")
+                        .and()
+                        .logout().logoutSuccessUrl("/");
+                http.cors().and().csrf().disable();
+            } catch (Exception e) {
+                logger.info("in exception customSecurityFilter "+e.getMessage());
+                throw new RuntimeException(e);
+
+            }
+        });
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
